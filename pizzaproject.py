be59bffbfa0ab02568f9fba881f1db08d6c301a3
timbosan <timbosan@Timothys-iMac.local>
2020-05-26 18:39:16 -0400

    firstname greeting

--- pizzaproject.py
+++ pizzaproject.py
@@ -1,1 +1,5 @@
 #Pizza Project for Code Louisville python may 2020
+
+first_name = input('Hello what is your name?  ')
+print("Hello ", first_name, " would you like to order a pizza?  Y/N")
+#order_pizza = input("Hello ", first_name, " would you like to order a pizza?  Y/N")
