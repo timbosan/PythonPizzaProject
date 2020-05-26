5fd405b969c68bba85d439188990443edaf8877d
timbosan <timbosan@Timothys-iMac.local>
2020-05-26 18:59:47 -0400

    added greeting and if person wants to order pizza

--- pizzaproject.py
+++ pizzaproject.py
@@ -1,5 +1,9 @@
 #Pizza Project for Code Louisville python may 2020
 
 first_name = input('Hello what is your name?  ')
-print("Hello ", first_name, " would you like to order a pizza?  Y/N")
-#order_pizza = input("Hello ", first_name, " would you like to order a pizza?  Y/N")
+order_pizza = input("Hello {} would you like to order a pizza?   Y/N   ".format(first_name)).upper()
+
+if order_pizza == "Y":
+    print("Awesome Sauce! What time slot on Friday works best for you?")
+else:
+    print("No worries! Have a great day.")
