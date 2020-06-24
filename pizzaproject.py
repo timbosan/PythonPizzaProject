cf501edab798e4abb4127b067f001869cb2aba0f
timbosan <timbosan@Timothys-iMac.local>
2020-06-24 08:06:33 -0400

    update still need to get exception working

--- pizzaproject.py
+++ pizzaproject.py
@@ -1,14 +1,17 @@
 ##Pi zza Project for Code Louisville python may 2020
 
+
 first_name = input('Hello what is your name?  ')
-order_pizza = input("Hello {} would you like to order a pizza?   Y/N   ".format(first_name)).upper()
+
 TimeSlots = ['6:00 - 6:15', '6:15 - 6:30', '6:30 - 6:45', '6:45 - 7:00']
+while True:
 try:
-    order_pizza != 'Y' or 'N'
+    order_pizza = input("Hello {} would you like to order a pizza?   Y/N   ".format(first_name)).upper()
 except NameError:
     print("Sorry, please type Y or N for your answer.")
-
-
+    continue
+else:
+    break
 if order_pizza == "Y":
     print("Awesome Sauce! What time slot on Friday works best for you?")
     length = len(TimeSlots)
