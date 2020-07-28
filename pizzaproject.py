a19ac979991ef6bcf6e22881c466cf10b20ecc13
timbosan <timbosan@Timothys-iMac.local>
2020-07-28 18:54:03 -0400

    whileloop

--- pizzaproject.py
+++ pizzaproject.py
@@ -17,14 +17,16 @@ def select_time():
 ##Function to select toppings for pizza
 def order_pizza():
     topping_choices = ['Pepperoni', 'Italian Sausage', 'Jalapenos', 'Mushrooms', 'Hot Honey','Basil', 'Finished Adding']
+    customer_topping_choices = []
     i = 0 
     print("\nWhat do you want on the pizza?\n")
-    ##while (user_topping_select != 8)
-    
+    ##while (user_topping_select != 7)
     for i, toppings in enumerate (topping_choices):
         print(f'\n{str(i+1)}. {toppings}\n')
     user_topping_select = input()
     user_topping_choice = int(user_topping_select) -1
+    customer_topping_choices.append(topping_choices[user_topping_choice])
+    customer_topping_choices_combined = ()
     print("\nGreat you've selected to top your pizza with {}!".format(topping_choices[user_topping_choice]))
     return(topping_choices[user_topping_choice])
 
